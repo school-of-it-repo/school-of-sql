6bddbe14edeca30fbb3451509bd64af816a1271a
School of IT <mail2schoolofit@gmail.com>
2023-03-13 08:11:45 +0530

    SQL workout Question No : 33

---
+++ Workout_No_33/ddl.sql
@@ -0,0 +1,16 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+Write an SQL query to count the occurrences each character.
+Order should be max. to min count and empty spaces should be display at last.
+*/
+
+
+select 'welcOme to School Of IT' as sentences;
