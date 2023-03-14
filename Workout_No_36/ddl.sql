52a5cf04d599b333d29b21ff4496c47359547740
School of IT <mail2schoolofit@gmail.com>
2023-03-14 09:06:52 +0530

    SQL workout Question No : 36

---
+++ Workout_No_36/ddl.sql
@@ -0,0 +1,30 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+UPrepare Point Table based results .
+Winners 4 pts , Losers – 0 pts , and Tie -  2 pts
+
+*/
+
+create table schoolofit.match_results(
+   team_a varchar(10),
+   team_b varchar(10),
+   result varchar(10)
+);
+
+insert into schoolofit.match_results values('india','bangladesh','india');
+insert into schoolofit.match_results values('india','pakistan','pakistan');
+insert into schoolofit.match_results values('india','srilanka',null);
+insert into schoolofit.match_results values('srilanka','bangladesh','bangladesh');
+insert into schoolofit.match_results values('srilanka','pakistan','pakistan');
+insert into schoolofit.match_results values('bangladesh','pakistan','pakistan');
+
+
+commit;
