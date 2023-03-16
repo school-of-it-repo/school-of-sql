3bbbbd5b53233179f82c480bec811c8d951ef4a6
School of IT <mail2schoolofit@gmail.com>
2023-03-16 11:37:51 +0530

    Answer for SQL workout Question No : 41

---
+++ Workout_No_41/SQL_Workout_No_41_Answer.sql
@@ -0,0 +1,66 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+Problem statement: we found that lot of duplicate entry made in ‘interest’ column in the table.
+Write an SQL query to remove duplicate entry and keep unique values.
+
+*/
+
+create table schoolofit.remove_duplicate_value
+(id serial
+,name varchar(50)
+,interest varchar(1000)
+);
+
+insert into schoolofit.remove_duplicate_value
+(name,interest)
+values
+('Person-A','shopping,gaming,watching-movies,football,shopping'),
+('Person-B','gaming,watching-movies,football'),
+('Person-C','gaming,watching-movies,football,gaming'),
+('Person-D','gaming,watching-movies,football,shopping'),
+('Person-E','reading,gaming'),
+('Person-F','shopping,reading,watching-movies,football,reading'),
+('Person-G','chatting,chatting,watching-movies,chatting,chatting'),
+('Person-H','gaming,gaming,watching-movies,gaming,shopping');
+
+/
+
+select
+	id
+	,name
+	,interest
+from
+	schoolofit.remove_duplicate_value;
+
+/
+
+-- Answer
+
+with _data as
+(
+	select
+	id
+	,name
+	,unnest(string_to_array( interest,',')) as _interest
+ from
+ 	schoolofit.remove_duplicate_value
+ )
+ 	select
+
+ 	id
+ 	,name
+ 	,string_agg(distinct _interest,',' order by _interest desc)  as interest
+  from
+  	_data
+  group by id,name
+  order by id
+;
