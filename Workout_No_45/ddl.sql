04f365039839a59cf77d8f62cdd7626cb609099b
School of IT <mail2schoolofit@gmail.com>
2023-03-17 18:35:28 +0530

    SQL workout Question No : 45

---
+++ Workout_No_45/ddl.sql
@@ -0,0 +1,41 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+Problem statement: Write an SQL query to find the list of id’s have ONLY Null and empty (‘’)
+Status.
+
+
+*/
+
+create table schoolofit.user_request_12
+(
+	id int
+	,status varchar(20)
+);
+insert into schoolofit.user_request_12
+(id,status)
+values
+(1, null),
+(2, ''),
+(3, 'a'),
+(4, null),
+(4, 'a'),
+(5, ''),
+(5, 'a'),
+(6, ''),
+(6, null),
+(6, null);select
+	id
+	,status
+from
+	schoolofit.user_request_12 ;
+/
+
