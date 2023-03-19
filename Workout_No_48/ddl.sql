32c9beafad56dc39f8b0031812c8834198cd9a9a
School of IT <mail2schoolofit@gmail.com>
2023-03-19 12:22:09 +0530

    SQL Workout Question No : 48

---
+++ Workout_No_48/ddl.sql
@@ -0,0 +1,46 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+
+Problem statement: Number of Calls between two person.
+
+
+*/
+
+create table schoolofit.call_tracker
+(
+log_id serial
+,from_id int
+,to_id int
+,call_duration int
+);
+
+insert into schoolofit.call_tracker
+(from_id,to_id,call_duration)
+values
+(1,2,12),
+(1,2,30),
+(1,5,20),
+(1,5,10),
+(1,4,20),
+(1,5,30),
+(2,1,20),
+(5,2,30),
+(10,8,40),
+(1,4,30);
+/
+
+select
+	*
+from
+	schoolofit.call_tracker;
+
+/
