a6425241d48af510bda7f99b0395bee70caf2399
School of IT <mail2schoolofit@gmail.com>
2023-03-29 21:33:54 +0530

    SQL Workout No : 78

---
+++ Workout_No_78/ddl.sql
@@ -0,0 +1,21 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+
+Problem statement: Print below expected output using Recursive
+
+create table schoolofit.letters(id varchar(20));
+insert into schoolofit.letters(id) values ('a'),('b');
+
+*/
+
+create table schoolofit.letters(id varchar(20));
+insert into schoolofit.letters(id) values ('a'),('b');
