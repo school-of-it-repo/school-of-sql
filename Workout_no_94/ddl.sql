26d1be973c6fe3b1d0771114abbfbb4a80f8ed39
School of IT <mail2schoolofit@gmail.com>
2023-04-10 09:28:45 +0530

    SQL Workout No : 94

---
+++ Workout_no_94/ddl.sql
@@ -0,0 +1,28 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+Problem statement: Prepare Report status based on in/out swipe status
+
+
+
+*/
+
+create table schoolofit.list_of_fruits
+(name varchar(20));
+
+insert into schoolofit.list_of_fruits
+values
+('apple'),('apple'),('apple'),('banana')
+,('banana'),('banana'),('orange'),('orange'),('watermelon'),('watermelon')
+,('apple'),('mango'),('plums');
+
+
+select * from 	schoolofit.list_of_fruits;
