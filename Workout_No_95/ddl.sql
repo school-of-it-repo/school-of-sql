46fb19043fcea43278ea9ad1a834a772252cef9e
School of IT <mail2schoolofit@gmail.com>
2023-04-10 11:10:45 +0530

    SQL Workout No : 95

---
+++ Workout_No_95/ddl.sql
@@ -0,0 +1,44 @@
+/*
+**********************************
+Source from : School of IT
+Contact : +91 98403 26240
+Mail : mail2schoolofit@gmail.com
+**********************************
+
+-----------------
+Problem statement:
+-----------------
+
+Find # no of visited floor and maximum
+no. of visited floor and unique resources at each name level
+
+
+
+
+*/
+
+drop table schoolofit.max_floor_visit ;
+create table schoolofit.max_floor_visit
+(
+name varchar(20)
+,address varchar(30)
+,email varchar(40)
+,visited_floor int
+,resources varchar(30)
+);
+
+insert into schoolofit.max_floor_visit
+(name,address,email,visited_floor,resources)
+values
+('Person-A','Bangalore','Person-A@gmail.com',1,'CPU'),
+('Person-A','Bangalore','Person-A@gmail.com',1,'CPU'),
+('Person-A','Bangalore','Person-A@gmail.com',1,'DESKTOP'),
+('Person-A','Bangalore','Person-A@gmail.com',2,'DESKTOP'),
+('Person-A','Bangalore','Person-A@gmail.com',3,'CPU'),
+('Person-B','Bangalore','Person-B@gmail.com',1,'CPU'),
+('Person-B','Bangalore','Person-B@gmail.com',2,'CPU'),
+('Person-B','Bangalore','Person-B@gmail.com',2,'CPU'),
+('Person-C','Bangalore','Person-C@gmail.com',4,'DESKTOP'),
+('Person-C','Bangalore','Person-C@gmail.com',4,'DESKTOP');
+
+select * from schoolofit.max_floor_visit ;
